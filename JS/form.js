bd015d3db1553fab4fd8a82810c409ded0682477
VICTOR SANTOS <victor.f089@gmail.com>
2023-10-11 01:27:40 +0200

    characters left added, not finished

--- JS/form.js
+++ JS/form.js
@@ -1,5 +1,12 @@
 const form = document.querySelector('[data-js="form"]');
+const message = document.getElementById("your-question");
+const counter = document.getElementById("your-answer");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+});
+
+message.addEventListener("input", function () {
+  const messageLenght = message.value.lenght;
+  counter.textContent = `${messageLenght} characters left`;
 });
