fa4b43bab50c40231b39b046b8b3460f0ec63f50
VICTOR SANTOS <victor.f089@gmail.com>
2023-10-12 18:10:50 +0200

    card updated

--- JS/form.js
+++ JS/form.js
@@ -6,16 +6,36 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
+  console.log(data);
 
   const card = document.createElement("section");
+  card.classList.add("question-card");
+  document.body.append(card);
+
+  const img = document.createElement("img");
+  img.classList.add("question-card__bookmark");
+  img.setAttribute("src", "./img/bookmark.png");
+  card.append(img);
+
+  const h2 = document.createElement("h2");
+  h2.textContent = "Question";
+  card.append(h2);
 
   const questionHeading = document.createElement("p");
+
+  const button = document.createElement("button");
+
+  const div = document.createElement("div");
+
   questionHeading.textContent = data["your-question"];
+  button.textContent = data["your-answer"];
+
+  button.classList.add("question-card__button");
+  questionHeading.classList.add("question-card__answer");
+  div.classList.add("question-card__tags");
 
   card.append(questionHeading);
-  card.classList.add("question-card");
-
-  document.body.append(card, bookmark);
+  card.append(button);
 });
 
 message.addEventListener("input", function () {
